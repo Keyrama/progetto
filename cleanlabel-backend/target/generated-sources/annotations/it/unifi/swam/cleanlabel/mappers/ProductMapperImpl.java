45a3298925f5f461fc0c655334cba68d6b512002
IreneScarpanti <irenescarpanti@gmail.com>
2026-05-01 18:52:22 +0200

    aggiunti ingredienti e prodotti al db

--- cleanlabel-backend/target/generated-sources/annotations/it/unifi/swam/cleanlabel/mappers/ProductMapperImpl.java
+++ cleanlabel-backend/target/generated-sources/annotations/it/unifi/swam/cleanlabel/mappers/ProductMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-01T12:50:35+0200",
+    date = "2026-05-01T18:17:25+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.9 (Microsoft)"
 )
 @Component
